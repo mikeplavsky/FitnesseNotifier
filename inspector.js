0fe925f083e945f6d7001c844f8428e9b1e91fee
mikeplavsky <mike.plavsky@quest.com>
2011-01-10 23:00:44 +0300

    Now showing Test Status - if it is running

--- inspector.js
+++ inspector.js
@@ -8,15 +8,18 @@ if ( $( 'link' ).filter( function() { return $(this).attr( 'href' ).match( 'fitn
 			
 			$('#fn-checkTest').bind('testState', function(ev,res){
 
-				if (res.running) {
+				if (res.running && res.testButton.length) {
+					$("#fn-test-running").show();
 					res.testButton.hide();
 				}
 				else {
+					$("#fn-test-running").hide();
 					res.testButton.show();
 				}
 				
 			});
 
+			$('body').append('<div id="fn-test-running" style="display:none">Test is running</div>');
 			var checker = function() {checkTest( window.location.pathname, res.fitnesseSrv ); }
 			checker();
 			
